645023ebbbc47e875a11b2719c8dd7f571778733
sharon <151963804+a-ia@users.noreply.github.com>
2025-04-26 17:40:38 -0400

    Update 2_indexingMultiDimensionalArrays.cpp

--- codesignal/multidimensional_arrays/2_indexingMultiDimensionalArrays.cpp
+++ codesignal/multidimensional_arrays/2_indexingMultiDimensionalArrays.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
 
+/* 
+ * All indices in C++ arrays are 0-based. 
+ * Let's say you want to visit an apartment on the second floor (index 1) 
+ * and bring a package to the first unit (index 0) in this building. Here's how you can do it:
+ */
+    
 int main() {
     std::vector<std::vector<int>> array = {{1, 2, 3},
                                            {4, 5, 6},
@@ -11,3 +17,9 @@ int main() {
 
     return 0;
 }
+
+/*
+ * We visited the element 4 in the array by its position. 
+ * The number 1 inside the first square brackets refers to the second inner vector, 
+ * and 0 refers to the first element of that vector.
+ */
