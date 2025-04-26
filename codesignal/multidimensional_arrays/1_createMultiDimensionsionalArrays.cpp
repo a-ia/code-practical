a21fb2d6c30a5ed18107e6268325f029c357f348
sharon <151963804+a-ia@users.noreply.github.com>
2025-04-26 17:38:50 -0400

    Update 1_createMultiDimensionsionalArrays.cpp

--- codesignal/multidimensional_arrays/1_createMultiDimensionsionalArrays.cpp
+++ codesignal/multidimensional_arrays/1_createMultiDimensionsionalArrays.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 
+// Creating Multidimensions Arrays
+// You Create a 2D vector, which is an array
+// In this example, array is a 2-dimensional vector, just like a 3-story 'apartment building,' where every floor is an inner vector.
+
 int main() {
     // Creating a 2D vector
     std::vector<std::vector<int>> array = {{1, 2, 3},
